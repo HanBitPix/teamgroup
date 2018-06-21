25c49a875eeb69bc80444cccf80814d0a4f733d8
HanBitPix <hanjt517@gmail.com>
2018-06-20 17:21:57 -0700

    Finisihed the backend

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,6 @@
 'use strict';
 
 module.exports = {
-  mongoURI: 'mongodb://admin:admin123@ds163650.mlab.com:63650/teamgroup'
+  mongoURI: 'mongodb://admin:admin123@ds163650.mlab.com:63650/teamgroup',
+  secretOrKey: 'secret'
 };
